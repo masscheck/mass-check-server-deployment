71b533bc508ea848f219cef7419617ffeb994af9
Yong Ming-Jia <yongmingjia0112@gmail.com>
2021-05-24 15:33:13 +0800

    Fix CORS

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-const cors = require('cors');
+import cors from 'cors';
 // import path from 'path';
 // import enforce from 'express-sslify';
 
@@ -18,14 +18,24 @@ const app = express();
 //     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 //   });
 // }
-app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(cors());
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
 app.use('/api', downloadPrivateKeyRoute);
 app.use('/api', createAcc);
 app.use('/api', getUserInfo);
+
+app.use('/', (req, res, next) => {
+  res.send('MassCheck API');
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
